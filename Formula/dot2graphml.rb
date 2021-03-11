060954b59df15410003b75a3bbd212a85935e137
Dan Lipsitt <danlipsitt@gmail.com>
2021-03-11 15:23:04 -0800

    dot2graphml: fix audit errors.

--- Formula/dot2graphml.rb
+++ Formula/dot2graphml.rb
@@ -1,8 +1,7 @@
 class Dot2graphml < Formula
-  desc "This package contains a converter from DOT format used by GraphViz (http://graphviz.org) to variant of GraphML (http:/graphml.graphdrawing.org) used by yEd (http:/www.yworks.comen/products_yed_about.html)."
-  homepage "http://hackage.haskell.org/package/dot2graphml"
-  url "http://hackage.haskell.org/package/dot2graphml-0.1.0.2/dot2graphml-0.1.0.2.tar.gz"
-  version "0.1.0.2"
+  desc "Converter from GraphViz DOT format to yEd variant of GraphML"
+  homepage "https://hackage.haskell.org/package/dot2graphml"
+  url "https://hackage.haskell.org/package/dot2graphml-0.1.0.2/dot2graphml-0.1.0.2.tar.gz"
   sha256 "31e20e5aa13bd4735b7f21078dbcf8703e147b008805712ac8f13547c4e58ebb"
 
   livecheck do
@@ -19,6 +18,6 @@ class Dot2graphml < Formula
   end
 
   test do
-    assert_match /<\/graphml>/, shell_output("#{bin}/dot2graphml <<<'digraph g {}'")
+    assert_match %r{</graphml>}, shell_output("#{bin}/dot2graphml <<<'digraph g {}'")
   end
 end
